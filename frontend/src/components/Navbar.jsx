fa444d51a117738cc8d9db9867dbc23929487fcc
nathan karns <Nathan k>
2023-06-14 09:50:31 -0400

    fix responsive navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,57 +1,35 @@
-import React, { useState } from "react";
-import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
-const Navbar = () => {
-  const [nav, setNav] = useState(false);
-
-  const handleNav = () => {
-    setNav(!nav);
-  };
-
+const NavbarHeader = () => {
   return (
-    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white bg-black">
-      <h1 className="w-full text-3xl font-bold text-[#00df9a]">
-        LET'S FIT SWOLE
-      </h1>
-      <ul className="hidden md:flex">
-        <li className="p-4">
-          <a style={{ textDecoration: "none", color: "white" }} href="#Home">
-            Home
-          </a>
-        </li>
-        <li className="p-4">
-          <a style={{ textDecoration: "none", color: "white" }} href="#About">
-            About
-          </a>
-        </li>
-        <li className="p-4">
-          <a
-            style={{ textDecoration: "none", color: "white" }}
-            href="https://natekarnsportfolio.netlify.app/"
-          >
-            Contact
-          </a>
-        </li>
-      </ul>
-      <div onClick={handleNav} className="block md:hidden">
-        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
-      </div>
-      <ul
-        className={
-          nav
-            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
-            : "ease-in-out duration-500 fixed left-[-100%]"
-        }
-      >
-        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">
-          LET'S FIT SWOLE
-        </h1>
-        <li className="p-4 border-b border-gray-600">Home</li>
-        <li className="p-4 border-b border-gray-600">About</li>
-        <li className="p-4">Contact</li>
-      </ul>
-    </div>
+    <header className="py-2" id="navheader">
+      <Navbar expand="md" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand id="navBrand">LET'S FIT SWOLE</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link className="text-light" href="#Home">
+                Home
+              </Nav.Link>
+              <Nav.Link className="text-light" href="#About">
+                About
+              </Nav.Link>
+              <Nav.Link
+                className="text-light"
+                href="https://natekarnsportfolio.netlify.app/"
+              >
+                Contact
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 };
 
-export default Navbar;
+export default NavbarHeader;
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -2,13 +2,13 @@ import Cards from "../components/Cards";
 // import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Info from "../components/Info";
-import Navbar from "../components/Navbar";
+import NavbarHeader from "../components/Navbar";
 import Newslettter from "../components/Newslettter";
 
 function HomeScreen() {
   return (
     <div>
-      <Navbar />
+      <NavbarHeader />
       <Hero />
       <Info />
       <Newslettter />
