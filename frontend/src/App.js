9f93a3b75f28fb6b2fac7e81489173c53fa22a67
nathan karns <Nathan k>
2023-06-14 15:03:07 -0400

    add simple footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <Outlet />
       <ToastContainer />
+      <Footer />
     </div>
   );
 }
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,60 +1,72 @@
 import React from "react";
-import {
-  FaFacebookSquare,
-  FaGithubSquare,
-  FaInstagram,
-  FaTwitterSquare,
-} from "react-icons/fa";
+import { Col, Container, Row } from "react-bootstrap";
+// import {
+//   FaFacebookSquare,
+//   FaGithubSquare,
+//   FaInstagram,
+//   FaTwitterSquare,
+// } from "react-icons/fa";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div className="max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300 bg-black min-w-full">
-      <div>
-        <h1 className="w-full text-3xl font-bold text-[#00df9a]">
-          LET'S FIT SWOLE
-        </h1>
-        <p className="py-4">
-          The best fitness app on the market, guarenteed to help you reach your
-          goals.
-        </p>
-        <div className="flex justify-between md:w-[75%] my-6">
-          <FaFacebookSquare size={30} />
-          <FaInstagram size={30} />
-          <FaTwitterSquare size={30} />
-          <FaGithubSquare size={30} />
-        </div>
-      </div>
-      <div className="lg:col-span-2 flex justify-between mt-6">
-        <div>
-          <h6 className="font-medium text-gray-400">Home</h6>
-          <ul>
-            <li className="py-2 text-sm">Analytics</li>
-            <li className="py-2 text-sm">Marketing</li>
-            <li className="py-2 text-sm">Commerce</li>
-            <li className="py-2 text-sm">Insights</li>
-          </ul>
-        </div>
-        <div>
-          <h6 className="font-medium text-gray-400">About</h6>
-          <ul>
-            <li className="py-2 text-sm">Pricing</li>
-            <li className="py-2 text-sm">Documentation</li>
-            <li className="py-2 text-sm">Guides</li>
-            <li className="py-2 text-sm">API Status</li>
-          </ul>
-        </div>
-        <div>
-          <h6 className="font-medium text-gray-400">Contact</h6>
-          <ul>
-            <li className="py-2 text-sm">About</li>
-            <li className="py-2 text-sm">Blog</li>
-            <li className="py-2 text-sm">Jobs</li>
-            <li className="py-2 text-sm">Press</li>
-            <li className="py-2 text-sm">Careers</li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    // <div className="max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300 bg-black min-w-full">
+    //   <div>
+    //     <h1 className="w-full text-3xl font-bold text-[#00df9a]">
+    //       LET'S FIT SWOLE
+    //     </h1>
+    //     <p className="py-4">
+    //       The best fitness app on the market, guarenteed to help you reach your
+    //       goals.
+    //     </p>
+    //     <div className="flex justify-between md:w-[75%] my-6">
+    //       <FaFacebookSquare size={30} />
+    //       <FaInstagram size={30} />
+    //       <FaTwitterSquare size={30} />
+    //       <FaGithubSquare size={30} />
+    //     </div>
+    //   </div>
+    //   <div className="lg:col-span-2 flex justify-between mt-6">
+    //     <div>
+    //       <h6 className="font-medium text-gray-400">Home</h6>
+    //       <ul>
+    //         <li className="py-2 text-sm">Analytics</li>
+    //         <li className="py-2 text-sm">Marketing</li>
+    //         <li className="py-2 text-sm">Commerce</li>
+    //         <li className="py-2 text-sm">Insights</li>
+    //       </ul>
+    //     </div>
+    //     <div>
+    //       <h6 className="font-medium text-gray-400">About</h6>
+    //       <ul>
+    //         <li className="py-2 text-sm">Pricing</li>
+    //         <li className="py-2 text-sm">Documentation</li>
+    //         <li className="py-2 text-sm">Guides</li>
+    //         <li className="py-2 text-sm">API Status</li>
+    //       </ul>
+    //     </div>
+    //     <div>
+    //       <h6 className="font-medium text-gray-400">Contact</h6>
+    //       <ul>
+    //         <li className="py-2 text-sm">About</li>
+    //         <li className="py-2 text-sm">Blog</li>
+    //         <li className="py-2 text-sm">Jobs</li>
+    //         <li className="py-2 text-sm">Press</li>
+    //         <li className="py-2 text-sm">Careers</li>
+    //       </ul>
+    //     </div>
+    //   </div>
+    // </div>
+    <footer>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">
+            <p>LET'S FIT SWOLE &copy; {currentYear}</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   );
 };
 
